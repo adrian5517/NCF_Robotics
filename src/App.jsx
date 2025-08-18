348d66ca01c690248b43196ed9c04ebfbbc534e0
Adrian Boncodin <juelboncodin@gmail.com>
2025-08-19 06:07:19 +0800

    Added analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Hero from '../components/HeroPage';
 import About from '../components/AboutSection';
 import Achievements from '../components/AchievementsSection';
 import Footer from '../components/Footer';
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <About />
       <Achievements />
       <Footer />
+      <Analytics />
     </div>
   )
 }
